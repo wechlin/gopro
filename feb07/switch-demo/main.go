2933a3229bba242bc570e6c85368f4d8449bcb6d
Liam Echlin <wechlin@umbctraining.com>
2024-02-07 18:49:17 -0500

    Add empty switch expression and other assorted goodies

--- feb07/switch-demo/main.go
+++ feb07/switch-demo/main.go
@@ -2,12 +2,33 @@ package main
 
 import "fmt"
 
-func main() {
-	month := 2
+func fn() int {
+	return 6
+}
 
-	switch month {
+func main() {
+	// switch may take an short variable initialization
+	switch month := 6; month {
+	// default case should be first or last
+	default:
+		fmt.Println("31 days")
+	// Apparently a function call can be a case value, but don't
+	// expect to be efficient (no jump table :(
+	case 9, 4, fn(), 11:
+		fmt.Println("30 days")
 	case 2:
 		fmt.Println("28 days, usually")
 
+	}
+
+	x, y := 5, 7
+
+	switch {
+	case x > y:
+		fmt.Println("x > y")
+	case x == y:
+		fmt.Println("x = y")
+	case x < y:
+		fmt.Println("x < y")
 	}
 }
