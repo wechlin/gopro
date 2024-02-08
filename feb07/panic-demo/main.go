a63bd00bd2023c28d6474078530e18e1127aced6
Liam Echlin <wechlin@umbctraining.com>
2024-02-07 19:49:04 -0500

    Add demo of out-of-bounds panic

---
+++ feb07/panic-demo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Slices to be explained later
+	apostles := []string{"John", "Paul", "George", "Ringo"}
+
+	fmt.Println(apostles[5])
+
+	fmt.Println("Did not reach")
+}
