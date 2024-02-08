5657df0b57c639b30e1a27e5d4651a820a520e1c
Liam Echlin <wechlin@umbctraining.com>
2024-02-07 20:11:13 -0500

    Add example of recovering from a panic.

---
+++ feb07/recovery/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func get(values []float64, idx int) float64 {
+	defer func() {
+		if x := recover(); x != nil {
+			fmt.Println(x)
+			fmt.Println("But wait, we'll recover from it!")
+		}
+	}()
+
+	return values[idx]
+}
+
+func main() {
+	data := []float64{3.14, 2.71, 6.02}
+
+	fmt.Println(get(data, 2))
+	fmt.Println(get(data, 0))
+	fmt.Println(get(data, -1))
+}
