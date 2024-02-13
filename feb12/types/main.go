f8bc5d86f5354737963350de8c8ff370b4979820
Liam Echlin <wechlin@umbctraining.com>
2024-02-12 20:15:55 -0500

    Add initial demo of arrays and types

--- feb12/types/main.go
+++ feb12/types/main.go
@@ -7,16 +7,34 @@ type (
 	Kg float64
 )
 
-func main() {
+func arrayChunk() {
 	var distance Km
 	var length float64
 
+	var legs = [...]Km{1100.0, 32.7, 55.5}
+
+	copyLegs := legs
+
 	distance = 1100.0
 	length = 981.3
+
+	for n, val := range copyLegs {
+		fmt.Println(n, val)
+	}
+
+	legs[1]++
+	fmt.Println(legs)
+	fmt.Println(copyLegs)
+
+	fmt.Println("Length is", len(copyLegs))
 
 	if length == distance {
 		fmt.Println("length and distance are equal")
 	}
 
 	fmt.Println(distance)
+}
+
+func main() {
+	arrayChunk()
 }
