ca07fa25df6cb9ea3e955f7decd07e2074ef3324
Liam Echlin <wechlin@umbctraining.com>
2024-02-12 21:16:56 -0500

    Add channel demo

--- feb12/types/main.go
+++ feb12/types/main.go
@@ -75,7 +75,90 @@ func mapChunk() {
 	}
 }
 
+func sliceChunk() {
+	var fibs = [...]int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}
+	var slice []int = fibs[2:6]
+
+	fmt.Println(slice)
+
+	slice[3] = 33
+
+	fmt.Println(slice)
+	fmt.Println(fibs)
+
+	// 90-95% of the time, this is how a slice is created and used
+	// (via the make() builtin function)
+	cats := make([]string, 0)
+
+	// Making the underlying array have capacity means that initial appends
+	// to cats will modify that initial underlying array, so if the slice
+	// view that is returned is not replacing the original slice view,
+	// unexpected things will happen!
+	//cats := make([]string, 0, 4)
+
+	fmt.Println("Length is", len(cats))
+
+	fmt.Println("Cat capacity is", cap(cats))
+	dogs := append(cats, "puma", "lynx")
+
+	fmt.Println("cats:", cats)
+	fmt.Println("dogs:", dogs)
+
+	cats = append(cats, "leopardis", "uncia")
+
+	fmt.Println("cats:", cats)
+	fmt.Println("dogs:", dogs)
+
+	slice = append(slice, 108, 99)
+
+	fmt.Println(slice)
+	fmt.Println(fibs)
+}
+
+func channelChunk() {
+	var hopper chan string
+
+	if hopper == nil {
+		fmt.Println("Zero value for a channel is nil")
+	}
+
+	hopper = make(chan string, 3)
+
+	// Sending is a statement by itself
+	hopper <- "Complexity"
+
+	fmt.Println(<-hopper + " is not so bad")
+
+	sender(hopper)
+	receiver(hopper)
+
+	close(hopper)
+
+	val, ok := <-hopper
+
+	if !ok {
+		fmt.Println("Channel is closed")
+	} else {
+		fmt.Println("val is", val)
+	}
+}
+
+// Send-only channels
+func sender(input chan<- string) {
+	input <- "Value 1"
+}
+
+// Receive-only channels
+func receiver(output <-chan string) {
+	fmt.Println("In receiver:", <-output)
+
+	// Cannot send to a receive-only channel
+	//output <- "nope"
+}
+
 func main() {
 	//arrayChunk()
-	mapChunk()
+	//mapChunk()
+	//sliceChunk()
+	channelChunk()
 }
