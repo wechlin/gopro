0f2020884e0ded8272ac44e77434d258c2c96bbe
Liam Echlin <wechlin@umbctraining.com>
2024-02-19 20:10:18 -0500

    Add tests to IsEven() function

--- feb19/even/even.go
+++ feb19/even/even.go
@@ -1,10 +1,5 @@
 package even
 
 func IsEven(n int) bool {
-	switch n {
-	case 0, 2, 4, 6, 8, 10:
-		return true
-	default:
-		return false
-	}
+	return n%2 == 0
 }
--- feb19/even/even_test.go
+++ feb19/even/even_test.go
@@ -1,1 +1,23 @@
 package even
+
+import "testing"
+
+func TestEven(t *testing.T) {
+	evens := []int{-2, 0, 2, 1024, 8, 12}
+
+	for _, num := range evens {
+		if !IsEven(num) {
+			t.Errorf("IsEven(%d) = false; want true", num)
+		}
+	}
+}
+
+func TestOdd(t *testing.T) {
+	odds := []int{-1, 1, 17, 13, 1023, 65535, -17}
+
+	for _, num := range odds {
+		if IsEven(num) {
+			t.Errorf("IsEven(%d) = true; want false", num)
+		}
+	}
+}
