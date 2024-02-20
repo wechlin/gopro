a13ca24f2e90aabcd2bf07997743496deffc98cb
Liam Echlin <wechlin@umbctraining.com>
2024-02-19 19:03:53 -0500

    Convert sort.Sort to slices.SortFunc to sort results by letter.

--- feb19/ch2e2/main.go
+++ feb19/ch2e2/main.go
@@ -3,27 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
-	"sort"
+	"slices"
 )
-
-type letterCount struct {
-	letter rune
-	count  int
-}
-
-type freqResult []letterCount
-
-func (fr *freqResult) Len() int {
-	return len(*fr)
-}
-
-func (fr *freqResult) Less(i, j int) bool {
-	return (*fr)[i].letter < (*fr)[j].letter
-}
-
-func (fr *freqResult) Swap(i, j int) {
-	(*fr)[i], (*fr)[j] = (*fr)[j], (*fr)[i]
-}
 
 func main() {
 	frequency := make(map[rune]int)
@@ -34,13 +15,20 @@ func main() {
 		}
 	}
 
-	results := freqResult{}
+	type letterCount struct {
+		letter rune
+		count  int
+	}
+
+	results := make([]letterCount, 0)
 
 	for letter, count := range frequency {
 		results = append(results, letterCount{letter, count})
 	}
 
-	sort.Sort(&results)
+	slices.SortFunc(results, func(a, b letterCount) int {
+		return int(a.letter - b.letter)
+	})
 
 	for _, item := range results {
 		fmt.Println(string(item.letter), item.count)
