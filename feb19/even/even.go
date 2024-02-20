ff0ed2f7d4b83d5b666096cc5aaa5c4e452665f7
Liam Echlin <wechlin@umbctraining.com>
2024-02-19 20:57:40 -0500

    Add truncation to zero of Kilometer/Mile conversion on negative values

--- feb19/even/even.go
+++ feb19/even/even.go
@@ -7,5 +7,8 @@ func IsEven(n int) bool {
 type Kilometers float64
 
 func (km Kilometers) InMiles() float64 {
+	if km < 0 {
+		return 0
+	}
 	return float64(km * 0.621371)
 }
--- feb19/even/even_test.go
+++ feb19/even/even_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+// In VS Code, the "Go: Toggle Test Coverage" flag from the Command Palette
+// will enable code test coverage measurement as well as highlight any code
+// red/green as to its coverage
+
 func TestEven(t *testing.T) {
 	evens := []int{-2, 0, 2, 1024, 8, 12, 16}
 
@@ -34,6 +38,7 @@ func TestKmToMiles(t *testing.T) {
 		{0, 0, 0.0001},
 		{1.6095, 1, 0.0001},
 		{1, 0.621371, 0.0001},
+		{-10, 0, 0.000000001},
 	}
 
 	for _, item := range cases {
