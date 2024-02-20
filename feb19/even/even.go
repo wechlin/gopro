fc62fc9ac535039c91ad4d7570dc44c694e4c83c
Liam Echlin <wechlin@umbctraining.com>
2024-02-19 20:40:22 -0500

    Add Kilometers type and appropriate conversion test

--- feb19/even/even.go
+++ feb19/even/even.go
@@ -3,3 +3,9 @@ package even
 func IsEven(n int) bool {
 	return n%2 == 0
 }
+
+type Kilometers float64
+
+func (km Kilometers) InMiles() float64 {
+	return float64(km * 0.621371)
+}
--- feb19/even/even_test.go
+++ feb19/even/even_test.go
@@ -1,6 +1,9 @@
 package even
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestEven(t *testing.T) {
 	evens := []int{-2, 0, 2, 1024, 8, 12, 16}
@@ -18,6 +21,24 @@ func TestOdd(t *testing.T) {
 	for _, num := range odds {
 		if IsEven(num) {
 			t.Errorf("IsEven(%d) = true; want false", num)
+		}
+	}
+}
+
+func TestKmToMiles(t *testing.T) {
+	cases := []struct {
+		in  Kilometers
+		out float64
+		tol float64
+	}{
+		{0, 0, 0.0001},
+		{1.6095, 1, 0.0001},
+		{1, 0.621371, 0.0001},
+	}
+
+	for _, item := range cases {
+		if res := item.in.InMiles(); math.Abs(res-item.out) > item.tol {
+			t.Errorf("(%v).InMiles() = %f; want %f", item.in, res, item.out)
 		}
 	}
 }
