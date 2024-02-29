06cb800d33c94a60a277d9d8e83c0c7130d5587b
Liam Echlin <wechlin@umbctraining.com>
2024-02-28 19:50:31 -0500

    Add demo of value vs. references sent on channels

---
+++ feb28/goroutines/ex3/channels.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type object struct {
+	name string
+}
+
+func main() {
+	norman := object{"Norman"}
+
+	valueChannel := make(chan object)
+	done := make(chan bool)
+
+	go func(done chan bool) {
+		local := <-valueChannel
+		local.name = "Norbert"
+		fmt.Println(local)
+		done <- true
+	}(done)
+
+	valueChannel <- norman
+	<-done
+	fmt.Println(norman)
+
+	refChannel := make(chan *object)
+
+	go func(done chan bool) {
+		local := <-refChannel
+		local.name = "Nyman"
+		fmt.Println(local)
+		done <- true
+	}(done)
+
+	refChannel <- &norman
+	<-done
+	fmt.Println(norman)
+}
