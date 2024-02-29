bf192db8c862781499f436de894dfd7995bf8759
Liam Echlin <wechlin@umbctraining.com>
2024-02-28 19:32:22 -0500

    Add initial demo of goroutines and goroutine completion

---
+++ feb28/goroutines/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func greet(n int, finished chan bool) {
+	fmt.Println("Hello World", n)
+	// Send to channel
+	finished <- true
+}
+
+func main() {
+
+	var times = 5
+	done := make(chan bool)
+
+	for n := 0; n < times; n++ {
+		go greet(n, done)
+	}
+
+	for n := 0; n < times; n++ {
+		// Receive from channel
+		<-done
+	}
+
+}
