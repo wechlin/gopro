cfdb71a33ed8e666d33bdb527d4ca98e02f5b6d1
Liam Echlin <wechlin@umbctraining.com>
2024-03-04 20:52:18 -0500

    Fix race condition by doing unsafe actions all in a single line of code

--- mar04/dangermouse/dangermouse.go
+++ mar04/dangermouse/dangermouse.go
@@ -21,14 +21,7 @@ func main() {
 
 	ref += 5
 
-	var up = unsafe.Pointer(ptr)
-	up = unsafe.Pointer(obj)
-	ref = uintptr(up)
-	ref += unsafe.Sizeof(obj.Name)
-	// The linter ain't lyin', this is dangerous
-	up = unsafe.Pointer(ref)
+	var val = (*int)(unsafe.Add(unsafe.Pointer(obj), unsafe.Sizeof(obj.Name)))
 
-	var val = (*int)(up)
-
-	fmt.Println(ref, ptr, up, obj, *val)
+	fmt.Println(ref, ptr, *val)
 }
