cf4cf3d06b9f91f35e19211218f97134ff8f5fef
Liam Echlin <wechlin@umbctraining.com>
2024-03-04 19:00:27 -0500

    Add concurrent solution to ch6e2

--- mar04/ch6e2/recgrep.go
+++ mar04/ch6e2/recgrep.go
@@ -1,12 +1,59 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path"
+	"strings"
+	"sync"
 )
+
+var wc sync.WaitGroup
 
 func main() {
 	if len(os.Args) != 3 {
-		log.Fatalf("Usage %s <pattern> <path>\n", os.Args[0])
+		log.Fatalf("Usage %s <text> <path>\n", os.Args[0])
+	}
+	pattern := os.Args[1]
+	root := os.Args[2]
+	//TODO vet that `root` is a directory
+
+	fs.WalkDir(os.DirFS(root), ".", func(fname string, d fs.DirEntry, problem error) error {
+		if d.IsDir() {
+			return nil
+		}
+
+		wc.Add(1)
+		go searchFile(path.Join(root, fname), pattern)
+
+		return nil
+	})
+
+	wc.Wait()
+}
+
+func searchFile(fname, needle string) {
+	defer wc.Done()
+	fh, err := os.Open(fname)
+	if err != nil {
+		log.Printf("Unable to open %s: %v", fname, err)
+		return
+	}
+	defer fh.Close()
+
+	scanner := bufio.NewScanner(fh)
+	for scanner.Scan() {
+		haystack := scanner.Text()
+		if strings.Contains(haystack, needle) {
+			fmt.Println(fname)
+			return
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		log.Printf("Error reading %s: %v", fname, err)
 	}
 }
